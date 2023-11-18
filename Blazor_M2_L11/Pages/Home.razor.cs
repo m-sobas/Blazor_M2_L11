2ba8bc5bd6ffc1df9c8a789d4019c4fca44cbe4a
Mieczysław Sobaś <m.sobas@fejm.pl>
2023-11-18 19:57:13 +0100

    Add project files.

---
+++ Blazor_M2_L11/Pages/Home.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor_M2_L11.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public string? Parametr { get; set; }
+
+        public void GoToHomework()
+        {
+            NavigationManager.NavigateTo("/homework/" + Parametr);
+        }
+    }
+}
---
+++ Blazor_M2_L11/Pages/Homework.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor_M2_L11.Pages
+{
+    public partial class Homework
+    {
+        [Parameter]
+        public string? Header { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public void GoToHome()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
